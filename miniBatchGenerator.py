921e8044e30d27c7fb3227bc87ac910eb6248cd1
JasaZnidar <47790266+JasaZnidar@users.noreply.github.com>
2024-09-15 12:22:18 +0200

    fixed miniBatch

--- miniBatchGenerator.py
+++ miniBatchGenerator.py
@@ -1,4 +1,5 @@
 import json
+import copy
 
 with open("miniBatch.json", "r") as f:
     data = json.load(f)
@@ -17,20 +18,452 @@ names = {
 teams = ['home', 'away']
 cnt = 1
 
-template = data['matches']['template']
+template = {
+      "competition id": "1",
+      "date": 199680,
+      "lineup": {
+        "away": {
+          "1": {
+            "id": ""
+          },
+          "10": {
+            "id": ""
+          },
+          "11": {
+            "id": ""
+          },
+          "12": {
+            "id": ""
+          },
+          "13": {
+            "id": ""
+          },
+          "2": {
+            "id": ""
+          },
+          "3": {
+            "id": ""
+          },
+          "4": {
+            "id": ""
+          },
+          "5": {
+            "id": ""
+          },
+          "6": {
+            "id": ""
+          },
+          "7": {
+            "id": ""
+          },
+          "8": {
+            "id": ""
+          },
+          "9": {
+            "id": ""
+          }
+        },
+        "home": {
+          "1": {
+            "id": ""
+          },
+          "10": {
+            "id": ""
+          },
+          "11": {
+            "id": ""
+          },
+          "12": {
+            "id": ""
+          },
+          "13": {
+            "id": ""
+          },
+          "2": {
+            "id": ""
+          },
+          "3": {
+            "id": ""
+          },
+          "4": {
+            "id": ""
+          },
+          "5": {
+            "id": ""
+          },
+          "6": {
+            "id": ""
+          },
+          "7": {
+            "id": ""
+          },
+          "8": {
+            "id": ""
+          },
+          "9": {
+            "id": ""
+          }
+        }
+      },
+      "name": {
+        "away": "",
+        "home": ""
+      },
+      "plays": [
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 2,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 54
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 3,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 108
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 2,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "away",
+          "time": 162
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 4,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 216
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 5,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 270
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 3,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "away",
+          "time": 324
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 6,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 278
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 7,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 432
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 4,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "away",
+          "time": 486
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 8,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 540
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 9,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 594
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 5,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "away",
+          "time": 648
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 10,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 702
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 11,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 756
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 6,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "away",
+          "time": 810
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 12,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 864
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 2,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 918
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 7,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "away",
+          "time": 972
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 3,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 1026
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 4,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 1080
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 8,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "away",
+          "time": 1134
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 5,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 1188
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 6,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 1242
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 9,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "away",
+          "time": 1296
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 7,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 1350
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 8,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 1404
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 10,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "away",
+          "time": 1458
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 9,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 1512
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 10,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 1566
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 11,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "away",
+          "time": 1620
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 11,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 1674
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 12,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "home",
+          "time": 1728
+        },
+        {
+          "action": "goal scored",
+          "details": "",
+          "location": [0, 0],
+          "player_1": 12,
+          "player_2": 0,
+          "target": [0, 0],
+          "team": "away",
+          "time": 1782
+        }
+      ],
+      "result": {
+        "away": 11,
+        "home": 22
+      }
+    }
 
 for week in range(len(matches)):
     for match in range(len(matches[week])):
         
-        template['date'] = dates[week]
-        template['name']['home'] = names[matches[week][match][0]]
-        template['name']['away'] = names[matches[week][match][1]]
+        m = copy.deepcopy(template)
+        
+        m['date'] = dates[week]
+        m['name']['home'] = names[matches[week][match][0]]
+        m['name']['away'] = names[matches[week][match][1]]
         
         for team in range(2):
-            for player in range(len(template['lineup'][teams[team]])):
-                template['lineup'][teams[team]][str(player+1)]['id'] = str( (matches[week][match][team] - 1)*13 + player + 1 )
+            for player in range(len(m['lineup'][teams[team]])):
+                m['lineup'][teams[team]][str(player+1)]['id'] = str( (matches[week][match][team] - 1)*13 + player + 1 )
         
-        data['matches'][str(cnt)] = dict(template)
+        
+        print(f"{str(cnt)} => {m['name']['home']} vs. {m['name']['away']}")
+        data['matches'][str(cnt)] = dict(m)
+        print(f"\t {data['matches'][str(cnt)]['name']['home']} vs. {data['matches'][str(cnt)]['name']['away']}")
         cnt += 1
 
 with open("miniBatch.json", "w") as f:
